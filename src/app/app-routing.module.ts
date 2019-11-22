a7d8f6288be2025db7a0fb82ec661e17515f4553
Izabel Salvati <izabelsalvati@gmail.com>
2019-11-22 20:26:44 -0300

    continuacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaPlanoComponent } from './lista-plano/lista-plano.component';
+import { NovoPlanoComponent } from './novo-plano/novo-plano.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "listagem", component: ListaPlanoComponent},
+  { path: 'novo', component: NovoPlanoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
